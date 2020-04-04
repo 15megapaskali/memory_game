72e4a64abe2deaa18b7317408d037654bf5e0c87
15MPa <m.pajda@gmail.com>
2020-04-04 18:34:16 +0200

    main

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 import "../scss/main.scss";
+
+const $newGameBtn = $('#new-game-btn');
+const $optionsBtn = $('#options-btn');
+const $exitBtn = $('#exit-btn');
+
+$newGameBtn.click(()=>{
+    console.log("działa klik")
+    $('.main-menu').css("display","none")
+
+})
