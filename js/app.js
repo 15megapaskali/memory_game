0d437930956f6adf6be04a355cbb93ee3ae9b3ea
15MPa <m.pajda@gmail.com>
2020-04-05 10:57:29 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+
 import "../scss/main.scss";
+
+
 
 const $newGameBtn = $('#new-game-btn');
 const $optionsBtn = $('#options-btn');
@@ -7,5 +10,10 @@ const $exitBtn = $('#exit-btn');
 $newGameBtn.click(()=>{
     console.log("działa klik")
     $('.main-menu').css("display","none")
+    $('.game').css("display","block")
+})
 
+$(".back-btn").click(()=>{
+    $('.game').css("display","none")
+    $('.main-menu').css("display","block")
 })
